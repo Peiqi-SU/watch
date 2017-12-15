b1d8b0ba930a09c4fdad3a988331b48661b2aadd
peiqi <peiqi@peiqis-MacBook-Pro.local>
2017-12-15 02:57:12 -0500

    Finished balance watch

--- online_version/digitalWatch/sketch.js
+++ online_version/digitalWatch/sketch.js
@@ -7,11 +7,12 @@ var myGlobalDef = {
   m: 0,
   s: 0,
   nightMode: true,
+  scaleK: 0.0,
 };
 
 var myDefLine = {
   px: 0,
-  thickness_vary: true,
+  thickness_vary: false,
   thickness_begin: 1,
   thickness_end: 5,
   lineDist: 0,
@@ -24,16 +25,19 @@ var myDefBalance = {
   s: 0,
   x0: 0,
   y0: 0,
-  x1: 0,
-  x2: 0,
-  x3: 0,
-  x4: 0,
-  y1: 0,
-  y2: 0,
-  y3: 0,
-  y4: 0,
+  // x1: 0,
+  // x2: 0,
+  // x3: 0,
+  // x4: 0,
+  // y1: 0,
+  // y2: 0,
+  // y3: 0,
+  // y4: 0,
   diaText: 0,
   myFont: 0,
+  myFontSize: 22,
+  myFontSizeToTop: 7, // myFontSize * 0.3
+  myFontSizeToBtm: 12,// myFontSize * 0.5
 
   current_radians: 0,
   delta_radians: 0,
@@ -46,14 +50,14 @@ var myDefBalance = {
 
   // tangent
   pg: 0,
-  moffset: 0,
-  mnew_offset: 0,
-  hoffset: 0,
-  hnew_offset: 0,
-  mnear_x: 0.0,
-  mnear_y: 0.0,
-  hnear_x: 0.0,
-  hnear_y: 0.0,
+  // moffset: 0,
+  // mnew_offset: 0,
+  // hoffset: 0,
+  // hnew_offset: 0,
+  // mnear_x: 0.0,
+  // mnear_y: 0.0,
+  // hnear_x: 0.0,
+  // hnear_y: 0.0,
 };
 
 var myDefDot = {
@@ -109,10 +113,9 @@ function basicUi() {
   ellipse(myDefDot.px, myGlobalDef.watchY, myGlobalDef.dia);
 
   // display time
-  var myTextSize = (windowWidth / 32);
-  textSize(myTextSize);
+  textSize(32 * myGlobalDef.scaleK);
   textAlign(LEFT);
-  var DisplayH = myGlobalDef.h % 12 + 1;
+  var DisplayH = myGlobalDef.h % 12;
   DisplayH = ("0" + DisplayH).slice(-2);
   var DisplayM = ("0" + myGlobalDef.m).slice(-2);
   var DisplayS = ("0" + myGlobalDef.s).slice(-2);
@@ -129,13 +132,18 @@ function uiUpdate() {
   myDefBalance.px = windowWidth / 2;
   myDefDot.px = windowWidth / 6 * 5;
 
+  // update scale k
+  myGlobalDef.scaleK = windowWidth / 1000;
+
   // line watch
   myDefLine.lineDist = myGlobalDef.dia / 61.0;
+  myDefLine.thickness_begin = 1 * myGlobalDef.scaleK;
+  myDefLine.thickness_end = 4 * myGlobalDef.scaleK;
 
   // balance watch
   myDefBalance.x0 = myDefBalance.px;
   myDefBalance.y0 = myGlobalDef.watchY;
-  myDefBalance.diaText = myGlobalDef.dia * 0.85;
+  myDefBalance.diaText = myGlobalDef.dia * 0.8;
 
   myDefBalance.delta_radians1 = TWO_PI / (122 * 60);
   myDefBalance.delta_radians2 = PI / (48 * 60 + 58);
@@ -146,7 +154,8 @@ function uiUpdate() {
   myDefBalance.hoffset = createVector(1000.0, 1000.0, 1000.0);
   myDefBalance.hnew_offset = createVector(1000.0, 1000.0, 1000.0);
 
-  calculate_pos_balance_watch();
+  // myGetTime();
+  //   calculate_pos_balance_watch();
 }
 
 function drawLineWatch() {
@@ -159,7 +168,7 @@ function drawLineWatch() {
 
   for (var i = 0; i <= myGlobalDef.m; i++) {
     if (myDefLine.thickness_vary) {
-      var thickness = (myDefLine.thickness_end - myDefLine.thickness_begin) / 60.0 * i + 0.5;
+      var thickness = (myDefLine.thickness_end - myDefLine.thickness_begin) / 60.0 * i + 0.2 * myGlobalDef.scaleK;
       strokeWeight(thickness);
     } else {
       strokeWeight(myDefLine.thickness_begin);
@@ -181,17 +190,18 @@ function drawBalanceWatch() {
   calculate_pos_balance_watch();
   my_draw_clock_lines_balance_watch(myDefBalance.h, myDefBalance.m);
 
-  //image(myDefBalance.pg,0,0);
+// for debug ///////////////////////////////////////////////////////////////////////////////
+  //image(myDefBalance.pg, 0, 0);
 
   // reset variables
-  myDefBalance.moffset.set(1000.0, 1000.0, 1000.0);
-  myDefBalance.mnew_offset.set(1000.0, 1000.0, 1000.0);
-  myDefBalance.mnear_x = 0.0;
-  myDefBalance.mnear_y = 0.0;
-  myDefBalance.hoffset.set(1000.0, 1000.0, 1000.0);
-  myDefBalance.hnew_offset.set(1000.0, 1000.0, 1000.0);
-  myDefBalance.hnear_x = 0.0;
-  myDefBalance.hnear_y = 0.0;
+  // myDefBalance.moffset.set(1000.0, 1000.0, 1000.0);
+  // myDefBalance.mnew_offset.set(1000.0, 1000.0, 1000.0);
+  // myDefBalance.mnear_x = 0.0;
+  // myDefBalance.mnear_y = 0.0;
+  // myDefBalance.hoffset.set(1000.0, 1000.0, 1000.0);
+  // myDefBalance.hnew_offset.set(1000.0, 1000.0, 1000.0);
+  // myDefBalance.hnear_x = 0.0;
+  // myDefBalance.hnear_y = 0.0;
 
 }
 
@@ -217,6 +227,7 @@ function calculate_pos_balance_watch() {
       myDefBalance.current_radians = (myDefBalance.accum_sec - myDefBalance.sec_begin) * myDefBalance.delta_radians2 + PI;
     } else if (myDefBalance.accum_sec < myDefBalance.sec_end) { // 1:00-1:00:59
       myDefBalance.current_radians = (myDefBalance.accum_sec - myDefBalance.sec_end) * myDefBalance.delta_radians2;
+
     } else {
       myDefBalance.current_radians = (myDefBalance.accum_sec - myDefBalance.sec_end) * myDefBalance.delta_radians1;
     }
@@ -245,47 +256,60 @@ function my_draw_clock_lines_balance_watch(myHour, myMin) {
   // text is alway on white part
 
   // calculate text position on pgraphic
-  calculatePG_balance_watch(myDefBalance.current_radians, myGlobalDef.dia / 2, myGlobalDef.dia / 2 * 0.85);
+  var calculatedHourMinCoord = calculatePG_balance_watch(myDefBalance.current_radians, myGlobalDef.dia / 2, myGlobalDef.dia / 2 * 0.8, myHour, myMin);
 
-  myDefBalance.x3 = myDefBalance.x3 + (myDefBalance.hoffset.x - myDefBalance.hnear_x);
-  myDefBalance.y3 = myDefBalance.y3 + (myDefBalance.hoffset.y - myDefBalance.hnear_y);
-  myDefBalance.x4 = myDefBalance.x4 + (myDefBalance.moffset.x - myDefBalance.mnear_x);
-  myDefBalance.y4 = myDefBalance.y4 + (myDefBalance.moffset.y - myDefBalance.mnear_y);
+   //console.log("0:"+calculatedHourMinCoord[0] + "   1:"+calculatedHourMinCoord[1] + "   2:"+calculatedHourMinCoord[1] + "   3:"+calculatedHourMinCoord[1])
+
+  // myDefBalance.x3 = myDefBalance.x3 + (myDefBalance.hoffset.x - myDefBalance.hnear_x);
+  // myDefBalance.y3 = myDefBalance.y3 + (myDefBalance.hoffset.y - myDefBalance.hnear_y);
+  // myDefBalance.x4 = myDefBalance.x4 + (myDefBalance.moffset.x - myDefBalance.mnear_x);
+  // myDefBalance.y4 = myDefBalance.y4 + (myDefBalance.moffset.y - myDefBalance.mnear_y);
 
   if (myGlobalDef.nightMode) {
     fill(0);
   } else {
     fill(255);
   }
-
-  textFont(myFont, 32);
+  textFont(myFont);
+  var textSizeTempt = myDefBalance.myFontSize * myGlobalDef.scaleK;
+  textSize(textSizeTempt);
   textAlign(CENTER, CENTER);
-  text(myHour, myDefBalance.x3, myDefBalance.y3);
-  text(myMin, myDefBalance.x4, myDefBalance.y4);
-  fill(255, 0, 0);
-  // text(myHour, myDefBalance.x3 - 10000, myDefBalance.y3 - 10000);
-  // text(myMin, myDefBalance.x4 + 100, myDefBalance.y4 + 500);
-
-
+  text(myHour, calculatedHourMinCoord[0], calculatedHourMinCoord[1]);
+  text(myMin, calculatedHourMinCoord[2], calculatedHourMinCoord[3]);
+//console.log(calculatedHourMinCoord);
 }
 
-function calculatePG_balance_watch(to_radians, r, r_text) {
-  var mbx1, mby1, mbx2, mby2; // min box barder
-  var hbx1, hby1, hbx2, hby2; // hour box barder
+function calculatePG_balance_watch(to_radians, r, r_text, myHour, myMin) {
+  var mbx1, mby1, mbx2, mby2; // min box border
+  var hbx1, hby1, hbx2, hby2; // hour box border
+  var linex1, liney1, linex2, liney2; // line (x1, y1, x2, y2)
+  var hourMinCoord = [0.0, 0.0, 0.0, 0.0]; // (hourX, hourY, minX, minY) replace previous: hour(x3, y3) min (x4, y4)
+
+  // min and hour offset
+  var moffset = createVector(1000.0, 1000.0, 1000.0);
+  var mnew_offset = createVector(1000.0, 1000.0, 1000.0);
+  var hoffset = createVector(1000.0, 1000.0, 1000.0);
+  var hnew_offset = createVector(1000.0, 1000.0, 1000.0);
+
+  // min and hour nearest distance
+  var mnear_x = 0.0;
+  var mnear_y = 0.0;
+  var hnear_x = 0.0;
+  var hnear_y = 0.0;
 
   myDefBalance.pg.background(20, 0, 0);
 
   // line (x1, y1, x2, y2)
-  myDefBalance.x1 = myDefBalance.x0 + cos(to_radians) * r;
-  myDefBalance.y1 = myDefBalance.y0 + sin(to_radians) * r;
-  myDefBalance.x2 = myDefBalance.x0 - cos(to_radians) * r;
-  myDefBalance.y2 = myDefBalance.y0 - sin(to_radians) * r;
+  linex1 = myDefBalance.x0 + cos(to_radians) * r;
+  liney1 = myDefBalance.y0 + sin(to_radians) * r;
+  linex2 = myDefBalance.x0 - cos(to_radians) * r;
+  liney2 = myDefBalance.y0 - sin(to_radians) * r;
 
   // min (x4, y4) hour(x3, y3)
-  myDefBalance.x3 = myDefBalance.x0 + cos(to_radians + PI / 18) * r_text;
-  myDefBalance.y3 = myDefBalance.y0 + sin(to_radians + PI / 18) * r_text;
-  myDefBalance.x4 = myDefBalance.x0 - cos(to_radians - PI / 18) * r_text;
-  myDefBalance.y4 = myDefBalance.y0 - sin(to_radians - PI / 18) * r_text;
+  hourMinCoord[0] = myDefBalance.x0 + cos(to_radians + PI / 15) * r_text;
+  hourMinCoord[1] = myDefBalance.y0 + sin(to_radians + PI / 15) * r_text;
+  hourMinCoord[2] = myDefBalance.x0 - cos(to_radians - PI / 15) * r_text;
+  hourMinCoord[3] = myDefBalance.y0 - sin(to_radians - PI / 15) * r_text;
 
   // line
   myDefBalance.pg.fill(255, 0, 0);
@@ -293,80 +317,103 @@ function calculatePG_balance_watch(to_radians, r, r_text) {
   myDefBalance.pg.arc(myDefBalance.x0, myDefBalance.y0, r * 2, r * 2, (myDefBalance.current_radians - PI), (myDefBalance.current_radians), PIE);
 
   // text
-  myDefBalance.pg.textFont(myFont, 32);
+  var textSizeTempt = myDefBalance.myFontSize * myGlobalDef.scaleK;
+  myDefBalance.pg.textFont(myFont, textSizeTempt);
   myDefBalance.pg.textAlign(CENTER, CENTER);
   myDefBalance.pg.fill(0, 255, 0);
-  myDefBalance.pg.text(myDefBalance.h, myDefBalance.x3, myDefBalance.y3);
-  myDefBalance.pg.text(myDefBalance.m, myDefBalance.x4, myDefBalance.y4);
+  myDefBalance.pg.text(myHour, hourMinCoord[0], hourMinCoord[1]);
+  myDefBalance.pg.text(myMin, hourMinCoord[2], hourMinCoord[3]);
 
   // box of the text
-  var hcw = int(myDefBalance.pg.textWidth(str(myDefBalance.h)));
-  var mcw = int(myDefBalance.pg.textWidth(str(myDefBalance.m)));
+  var hcw = int(myDefBalance.pg.textWidth(str(myHour)));
+  var mcw = int(myDefBalance.pg.textWidth(str(myMin)));
 
   myDefBalance.pg.noFill();
   myDefBalance.pg.stroke(255);
-  hbx1 = int(myDefBalance.x3 - hcw / 2);
-  hby1 = int(myDefBalance.y3 - 18);
-  hbx2 = int(myDefBalance.x3 + hcw / 2);
-  hby2 = int(myDefBalance.y3 + 18);
-  mbx1 = int(myDefBalance.x4 - mcw / 2);
-  mby1 = int(myDefBalance.y4 - 18);
-  mbx2 = int(myDefBalance.x4 + mcw / 2);
-  mby2 = int(myDefBalance.y4 + 18);
+  hbx1 = int(hourMinCoord[0] - hcw / 2);
+  hby1 = int(hourMinCoord[1] - myDefBalance.myFontSizeToTop * myGlobalDef.scaleK);
+  hbx2 = int(hourMinCoord[0] + hcw / 2);
+  hby2 = int(hourMinCoord[1] + myDefBalance.myFontSizeToBtm * myGlobalDef.scaleK);
+  mbx1 = int(hourMinCoord[2] - mcw / 2);
+  mby1 = int(hourMinCoord[3] - myDefBalance.myFontSizeToTop * myGlobalDef.scaleK);
+  mbx2 = int(hourMinCoord[2] + mcw / 2);
+  mby2 = int(hourMinCoord[3] + myDefBalance.myFontSizeToBtm * myGlobalDef.scaleK);
 
   // debug
   myDefBalance.pg.rectMode(CORNERS);
   myDefBalance.pg.noFill();
-  myDefBalance.pg.stroke(255);
+  myDefBalance.pg.stroke(125);
   myDefBalance.pg.strokeWeight(1);
   myDefBalance.pg.rect(hbx1, hby1, hbx2, hby2);
   myDefBalance.pg.stroke(125);
   myDefBalance.pg.rect(mbx1, mby1, mbx2, mby2);
-console.log((hbx2-hbx1) + ":"+(hby2-hby1) + "-----"+ (mbx2-mbx1) + ":"+(mby2-mby1));
-console.log(mbx1 + "-" +mbx2);
 
-// console.log(mbx1 +" -- "+mbx2 );
-  // get every pixel color for min
-  for (var i = mbx1; i < mbx2; i+=6) {
-    for (var j = mby1; j < mby2; j+=6) {
-      var c = myDefBalance.pg.get(i, j);
-      // if it's a black pixel
-      if (green(c) > 125) {
-        // calculate the dist
-        myDefBalance.mnew_offset = getDistance_balance_watch(myDefBalance.x1, myDefBalance.y1, myDefBalance.x2, myDefBalance.y2, i, j);
-        //println("new_offset ", new_offset.z, "---offset", offset.z);
-
-        // if the dist is smaller than saved one, replace it
-        if (myDefBalance.mnew_offset.z < myDefBalance.moffset.z) {
-          myDefBalance.moffset = myDefBalance.mnew_offset.copy();
-          myDefBalance.mnear_x = i;
-          myDefBalance.mnear_y = j;
-          //println(i, j);
-        }
-      }
-    }
-  }
+  // get a image of current pg
+  myDefBalance.pg.loadPixels();
+  var d = pixelDensity();
   
-  // get every pixel color for hour
-  for (var k = hbx1; k < hbx2; k+=6) {
-    for (var l = hby1; l < hby2; l+=6) {
-      var c2 = myDefBalance.pg.get(k, l);
-      // if it's a black pixel
-      if (green(c2) > 125) {
-        // calculate the dist
-        myDefBalance.hnew_offset = getDistance_balance_watch(myDefBalance.x1, myDefBalance.y1, myDefBalance.x2, myDefBalance.y2, k, l);
-        //println("new_offset ", new_offset.z, "---offset", offset.z);
+  // get every pixel color for min
+  for (var i = mbx1; i < mbx2; i ++) {
+    for (var j = mby1; j < mby2; j ++) {
+      // var c = myDefBalance.pg.get(i, j);
+      for (var k = 0; k < d; k++) {
+        for (var l = 0; l < d; l++) {
+          // loop over
+          var idx = 4 * ((j * d + l) * width * d + (i * d + k));
+          var myGreen = myDefBalance.pg.pixels[idx + 1]; //green()
+          // if it's a green pixel
+          if (myGreen > 200) {
+            // calculate the dist
+            mnew_offset = getDistance_balance_watch(linex1, liney1, linex2, liney2, i, j);
+            //println("new_offset ", new_offset.z, "---offset", offset.z);
 
-        // if the dist is smaller than saved one, replace it
-        if (myDefBalance.hnew_offset.z < myDefBalance.hoffset.z) {
-          myDefBalance.hoffset = myDefBalance.hnew_offset.copy();
-          myDefBalance.hnear_x = k;
-          myDefBalance.hnear_y = l;
-          //println(i, j);
+            // if the dist is smaller than saved one, replace it
+            if (mnew_offset.z < moffset.z) {
+              moffset = mnew_offset.copy();
+              mnear_x = i;
+              mnear_y = j;
+              //println(i, j);
+            }
+          }
         }
       }
     }
   }
+
+  // get every pixel color for hour
+  for (var i = hbx1; i < hbx2; i ++) {
+    for (var j = hby1; j < hby2; j ++) {
+      // var c = myDefBalance.pg.get(i, j);
+      for (var k = 0; k < d; k++) {
+        for (var l = 0; l < d; l++) {
+          // loop over
+          var idx = 4 * ((j * d + l) * width * d + (i * d + k));
+          var myGreen = myDefBalance.pg.pixels[idx + 1]; //green()
+          // if it's a green pixel
+          if (myGreen > 200) {
+            // calculate the dist
+            hnew_offset = getDistance_balance_watch(linex1, liney1, linex2, liney2, i, j);
+            //println("new_offset ", new_offset.z, "---offset", offset.z);
+
+            // if the dist is smaller than saved one, replace it
+            if (hnew_offset.z < hoffset.z) {
+              hoffset = hnew_offset.copy();
+              hnear_x = i;
+              hnear_y = j;
+              //println(i, j);
+            }
+          }
+        }
+      }
+    }
+  }
+
+  hourMinCoord[0] = hourMinCoord[0] + (hoffset.x - hnear_x);
+  hourMinCoord[1] = hourMinCoord[1] + (hoffset.y - hnear_y);
+  hourMinCoord[2] = hourMinCoord[2] + (moffset.x - mnear_x);
+  hourMinCoord[3] = hourMinCoord[3] + (moffset.y - mnear_y);
+
+  return hourMinCoord;
 }
 
 function getDistance_balance_watch(lx1, ly1, lx2, ly2, dotx, doty) {
